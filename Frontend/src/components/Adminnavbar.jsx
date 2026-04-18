5df61f0d72721a9980df2e6ccb239f8a1a6c7656
vithun-sk <vithunkumar07@gmail.com>
2026-04-18 13:20:54 +0530

    admin nav changes

--- Frontend/src/components/Adminnavbar.jsx
+++ Frontend/src/components/Adminnavbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
-import profileImage from '../assets/ProfileImage.png'
+import { IoIosLogOut } from "react-icons/io";
 import '../Styles/Navbar.css'
  
 const AdminNavbar = () => {
     const navigate = useNavigate();
     return (
-        <nav className='hr-navBar'>
+        <nav className='admin-navBar'>
             <div className='website-name'>
                 <h1>Recruit<i style={{ color: '#d97706' }}>Hub</i> <span style={{fontSize:'14px', color:'#d97706', fontWeight:'600'}}>ADMIN</span></h1>
             </div>
@@ -15,12 +15,12 @@ const AdminNavbar = () => {
                 <NavLink className='list' to='/admin/jobs'><span>Jobs</span></NavLink>
                 <NavLink className='list' to='/admin/users'><span>Users</span></NavLink>
             </div>
-            <div className='nav-profile' onClick={() => {
+            <div className='admin-nav-profile' onClick={() => {
                 localStorage.removeItem('loggedInUser');
                 localStorage.removeItem('token');
                 navigate('/');
-            }} style={{cursor:'pointer', display:'flex', alignItems:'center', gap:'8px', color:'#f1f5f9', fontSize:'16px'}}>
-                <img src={profileImage} alt="img" style={{ width: '45px', height: '45px', borderRadius: '50%' }} />
+            }}style={{width:'50px', height:'50px',borderRadius:'50%' , display:'flex', justifyContent:'center', alignItems:'center'}}>
+                <IoIosLogOut style={{ width: '30px', height: '30px', borderRadius: '50%' }}/>
             </div>
         </nav>
     )
